5c50775c5dff810285a2a3cde5c4d5fd1e01138d
Kotesh <y.koteshpatro.gmail.com>
2018-08-18 13:42:50 +0530

    Conversion service Comunicating With exchange Service

---
+++ src/main/java/com/springmicroservices/CurrencyConversionServiceApplication.java
@@ -0,0 +1,17 @@
+package com.springmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+}
